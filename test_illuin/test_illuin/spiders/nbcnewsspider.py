458de077d471b2f7f0afe88515a5b9f399bd4611
Thomas Lefebvre <thomas.lefebvre@student-cs.fr>
2021-12-08 14:58:41 +0100

    Trying to use selenium

--- test_illuin/test_illuin/spiders/nbcnewsspider.py
+++ test_illuin/test_illuin/spiders/nbcnewsspider.py
@@ -1,13 +1,22 @@
 import scrapy
+from selenium import webdriver
 
 class nbcnewsspider(scrapy.Spider):
     name = 'nbcnews'
     start_urls = ['https://www.nbcnews.com/western-wildfires']
 
     def parse(self, response):
-        for articles in response.css('div.wide-tease-item__wrapper'):
+        driver = webdriver.Firefox()
+        driver.get(self.start_urls[0])
+        element = driver.find_elements_by_class_name("feeds__load-more-button")
+        element.click
+        html = element.page_source
+
+        for articles in html.css('div.wide-tease-item__wrapper'):
             yield {
                 'title': articles.css('h2.wide-tease-item__headline::text').get(),
                 'link': articles.css('a').attrib['href'],
                 'date': articles.css('div.wide-tease-item__timestamp::text').get()
             }
+        
+        driver.quit()
