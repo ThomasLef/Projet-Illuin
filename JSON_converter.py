36a68ff5dc714b22ceba816d9bb677f906a065cf
Thomas Lefebvre <thomaslefebvre1999@orange.fr>
2022-03-10 11:13:06 +0100

    Update geckdriver and JSON converter

--- JSON_converter.py
+++ JSON_converter.py
@@ -21,7 +21,29 @@ def extraction_json():
     
     return(final_list)
 
+def extraction_lieu():
+    
+    traduction_dict = open_json_tag()
 
+    nombre_articles = 60
+    final_list = []
+    for file_num in range(nombre_articles):
+        with open('./annotations/article_number_' + str(file_num) + '.json', 'r', encoding="utf8") as json_file:
+            data = json.load(json_file)
+            whole_text = data['rawString']
+            word_list = data['annotations'][-1]['value']['tags']
+            annotated_list = []
+
+            for tag in word_list:
+                if traduction_dict[tag['tag']] == "l":
+                    word = ""
+                    for i in range(tag['begin'], tag['end'] + 1):
+                        word += data['tokenized'][i]['word'] + " "
+                    annotated_list.append(word)
+
+        final_list.append((whole_text,{"lieux": annotated_list}))
+    
+    return(final_list)
 
 def open_json_tag():
     traduction_dict = {}
@@ -30,3 +52,6 @@ def open_json_tag():
         for tag in data['tags']:
             traduction_dict[tag['id']] = tag['shortcut']
     return traduction_dict
+
+
+print(extraction_lieu()[0])
